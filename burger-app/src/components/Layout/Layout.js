bbf79fb6ff7f80925027b8ee09eb992ceb5823bf
Madeswaran <VEEJAYADEESH_MADESWARAN@homedepot.com>
2019-05-21 20:45:37 +0530

    toggle

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+class Layout extends Component {
+    state= {
+        showSideDrawer: true
+    };
 
-);
+    showSideDrawerHandler = () =>{
+        this.setState({showSideDrawer:false})
+        console.log("Backdrop Clicked ", this.state.showSideDrawer)
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.showSideDrawerHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
 
 export default Layout;
--- burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,20 +2,33 @@ import React from 'react';
 import classes from './SideDrawer.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import BackDrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Auxillary'
 
 const sideDrawer = (props) => {
-
+    let sideDrawerClasses = [classes.SideDrawer, classes.Close]
+    if (props.open) {
+        sideDrawerClasses = [classes.SideDrawer, classes.Open]
+        console.log(sideDrawerClasses);
+    }
+    else{
+        sideDrawerClasses = [classes.SideDrawer, classes.Close]
+        console.log(sideDrawerClasses);
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <BackDrop show={props.open} clicked={props.closed} />
+            <div className={sideDrawerClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+
+                <nav>
+                    <NavigationItems />
+                </nav>
+
             </div>
-
-            <nav>
-                <NavigationItems />
-            </nav>
-
-        </div>
+        </Aux>
     );
 }
 
